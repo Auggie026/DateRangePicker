6abcf07e986226d88a1fd8ff80fd3dddb0e92955
Augusto Castro <augustocesarcastro026@gmail.com>
2021-05-26 00:22:46 -0300

    added materialDatePicker and onPositiveButtonClickListener

--- app/src/main/java/com/example/daterangepicker/MainActivity.kt
+++ app/src/main/java/com/example/daterangepicker/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.daterangepicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.datepicker.MaterialDatePicker
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showDateRangePicker()
+    }
+
+
+    private fun showDateRangePicker(){
+
+        val dateRangePicker = MaterialDatePicker.Builder
+            .dateRangePicker()
+            .setTitleText("Select Date")
+            .build()
+
+        dateRangePicker.show(
+            supportFragmentManager,
+            "date_range_pucker"
+        )
+
+        dateRangePicker.addOnPositiveButtonClickListener { datePicked ->
+
+            val starDate = datePicked.first
+            val secondDate = datePicked.second
+
+            Toast.makeText(this,
+            "$starDate $secondDate",
+            Toast.LENGTH_SHORT).show()
+        }
     }
 }
